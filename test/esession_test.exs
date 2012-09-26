87946ad7415843e4342989292d92d9ec837df702
Yurii Rashkovskii <yrashk@gmail.com>
2012-09-25 23:13:04 -0700

    Add a very basic test for storages

--- test/esession_test.exs
+++ test/esession_test.exs
@@ -1,9 +1,23 @@
 Code.require_file "../test_helper.exs", __FILE__
 
-defmodule EsessionTest do
+defmodule Http.Session.Test.Storage.Basic do
   use ExUnit.Case
 
-  test "the truth" do
-    assert true
+  test "secure cookie storage" do 
+    test_storage(Http.Session.Storage.Secure.new key: "26skV8PjTfd9xpaVtzXCmMEaAkefXgUu")
+  end
+
+  test "ETS storage" do 
+    test_storage(Http.Session.Storage.ETS.new)
+  end
+
+  defp test_storage(storage) do
+    # No session identifier
+    session = Http.Session.Storage.get storage, ""
+    session = session.put :value, 1
+    assert session.get(:value) == 1
+    dump = Http.Session.Storage.dump storage, session
+    session = Http.Session.Storage.get storage, dump
+    assert session.get(:value) == 1
   end
 end
