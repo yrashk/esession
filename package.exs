cb62f5eef73a975b62c19d8d99a9384fa7329cb5
Yurii Rashkovskii <yrashk@gmail.com>
2012-09-28 22:28:08 -0700

    Add expm package

---
+++ package.exs
@@ -0,0 +1,4 @@
+Expm.Package.new(name: "esession", description: "HTTP session management",
+                 version: "0.0.1", keywords: ["HTTP","session"], 
+                 maintainers: [[name: "Yurii Rashkovskii", email: "yrashk@gmail.com"]],
+                 repositories: [[github: "yrashk/esession", tag: "0.0.1"]])
